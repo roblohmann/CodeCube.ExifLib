eeb945bcaf00d5122bd04d7dc0a7791ffa73e93c
Rob Lohmann <roblohman@gmail.com>
2019-07-27 15:12:37 +0200

    ported initial version from .NET2.x

---
+++ CodeCube.ExifLib/ExifLibException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodeCube.ExifLib
+{
+    public class ExifLibException : Exception
+    {
+        public ExifLibException()
+        {
+        }
+
+        public ExifLibException(string message)
+                : base(message)
+        {
+        }
+
+        public ExifLibException(string message, Exception innerException)
+                : base(message, innerException)
+        {
+        }
+    }
+}
